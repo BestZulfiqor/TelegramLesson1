f3cbc3adc8614b8c859d98f7836d314afb66e1de
Alimamatov Zulfiqor <theassassinaa@gmail.com>
2026-04-22 20:45:55 +0500

    now we can find last message

---
+++ User/Message.cs
@@ -0,0 +1,3 @@
+namespace TelegramBot.User;
+
+public record class Message(int Id, bool IsMedia);
--- User/UserData.cs
+++ User/UserData.cs
@@ -2,7 +2,8 @@ namespace TelegramBot.User;
 
 public class UserData
 {
-    public string StepikId { get; set; }
+    public string? StepikId { get; set; }
+    public Message? LastMessage { get; set; }
     public override string ToString()
     {
         return $"StepikId={StepikId}";
