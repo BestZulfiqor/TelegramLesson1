9548707aafaab29e4e503058c12f4d13e480ba64
Alimamatov Zulfiqor <theassassinaa@gmail.com>
2026-04-22 20:48:41 +0500

    add stack and condition for next and previous page

--- User/UserState.cs
+++ User/UserState.cs
@@ -2,4 +2,18 @@ using TelegramBot.User.Pages;
 
 namespace TelegramBot.User;
 
-public record class UserState(IPage Page, UserData UserData);
+public record class UserState(Stack<IPage> Pages, UserData UserData)
+{
+    public IPage CurrentPage => Pages.Peek();
+
+    /// <summary>
+    /// Добавляет страницу в стек только если страница того же типа ещё не находится в стеке
+    /// </summary>
+    public void AddPage(IPage page)
+    {
+        if (!Pages.Any(p => p.GetType() == page.GetType()))
+        {
+            Pages.Push(page);
+        }
+    }
+}
